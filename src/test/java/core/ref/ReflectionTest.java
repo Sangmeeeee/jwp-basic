044e7d88a203c8a5113f4e4c075c5e2bb9e006f5
Sangmeeeee <goodman1223@naver.com>
2023-02-21 02:00:19 +0900

    10.1.2.1 자바 리플렉션 API 활용해 클래스 정보 출력하기

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -7,6 +7,11 @@ import org.slf4j.LoggerFactory;
 import next.model.Question;
 import next.model.User;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
@@ -14,6 +19,20 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+        for(Field field : clazz.getFields())
+            logger.debug(field.getName());
+        for(Field field : clazz.getDeclaredFields())
+            logger.debug(field.getName());
+        for(Constructor<?> constructor : clazz.getDeclaredConstructors()){
+            StringBuilder sb = new StringBuilder();
+            for(Parameter parameter : constructor.getParameters())
+                sb.append("(" + parameter.getParameterizedType() + ")");
+            logger.debug("{} : {}", constructor.getName(), sb.toString());
+        }
+        for(Method method : clazz.getDeclaredMethods()){
+            logger.debug(method.getName());
+        }
+
     }
     
     @Test
