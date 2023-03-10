d9207db822c9ee2c29ac836f0a63a4496b48eb57
Sangmeeeee <goodman1223@naver.com>
2023-03-11 02:18:58 +0900

    10.1.3 요구사항 분리 및 힌트
    애노테이션 시간 매핑을 담당할 AnnotationHandlerMapping 클래스를 추가한 후 초기화한다.

--- src/main/java/core/nmvc/AnnotationHandlerMapping.java
+++ src/main/java/core/nmvc/AnnotationHandlerMapping.java
@@ -1,14 +1,20 @@
 package core.nmvc;
 
-import java.util.Map;
+import com.google.common.collect.Maps;
+import core.annotation.RequestMapping;
+import core.annotation.RequestMethod;
+import org.reflections.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
-
-import com.google.common.collect.Maps;
-
-import core.annotation.RequestMethod;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
     private Object[] basePackage;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -18,7 +24,20 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        ControllerScanner controllerScanner = new ControllerScanner(basePackage);
+        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+        for(Class<?> clazz : controllers.keySet()){
+            Set<Method> methods = ReflectionUtils.getAllMethods(clazz, ReflectionUtils.withAnnotation(RequestMapping.class));
+            for(Method method : methods){
+                RequestMapping rm = method.getAnnotation(RequestMapping.class);
+                HandlerExecution handlerExecution = new HandlerExecution(controllers.get(method.getDeclaringClass()), method);
+                handlerExecutions.put(createHandlerKey(rm), handlerExecution);
+            }
+        }
+    }
 
+    private HandlerKey createHandlerKey(RequestMapping rm){
+        return new HandlerKey(rm.value(), rm.method());
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
--- src/main/java/core/nmvc/HandlerExecution.java
+++ src/main/java/core/nmvc/HandlerExecution.java
@@ -4,9 +4,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import core.mvc.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
 
 public class HandlerExecution {
+    private static final Logger log = LoggerFactory.getLogger(HandlerExecution.class);
+    private Object declaredObject;
+    private Method method;
+
+    public HandlerExecution(Object declaredObject, Method method){
+        this.declaredObject = declaredObject;
+        this.method = method;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(declaredObject, request, response);
     }
 }
